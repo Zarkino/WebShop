39602b39579a68d2b1c09bdc8b67dfb9f88e1b5e
Zarkino <fhv2325@gmail.com>
2019-02-08 16:32:23 +0100

    New Database class

--- src/Connect.php
+++ src/Connect.php
@@ -16,4 +16,22 @@ $conn->set_charset("utf8");
 if(!$conn->select_db("webshop")) {
     print('Error connecting' . $conn->connect_error);
 }
+
+class Database {
+    private static $db;
+    private $conn;
+
+    private function create() {
+        $this->conn = new MySQLi('localhost', 'root', 'yes1');
+        self::$db->conn->set_charset("utf8");
+    }
+
+    private function destroy() {
+        $this->conn->close();
+    }
+
+    public static function connection() {
+        return self::$db->conn;
+    }
+}
 ?>
