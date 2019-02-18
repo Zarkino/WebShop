f19f7b50caf69687a86c74c7e7a6074603463f64
Zarkino <fhv2325@gmail.com>
2019-02-18 11:45:43 +0100

    Fix the new_?.php

--- src/new_product.php
+++ src/new_product.php
@@ -1,53 +1,45 @@
 <?php
-include('connect.php');
+include('database.php');
 
 if (isset($_POST['submit'])) {
-$productName = mysqli_real_escape_string($conn, $_POST['productName']);
-$prorductCatagory = mysqli_real_escape_string($conn, $_POST['prorductCatagory']);
-$productPrice = mysqli_real_escape_string($conn, $_POST['$productPrice']);
-$stock = mysqli_real_escape_string($conn, $_POST['stock']);
+    $name = mysqli_real_escape_string(connect(), $_POST['name']);
+    $category = mysqli_real_escape_string(connect(), $_POST['category']);
+    $price = mysqli_real_escape_string(connect(), $_POST['price']);
+    $stock = mysqli_real_escape_string(connect(), $_POST['stock']);
 
-
-//Tjekker for tomme felter
-if (empty($productName) || empty($prorductCatagory) || empty($productPrice) || empty($stock) ||  {
-    header("location: ./new_product.php?=empty");
-    exit();
-} else if (!preg_match("/^[a-zA-Z]*$/", $productName) || !preg_match("/^[a-zA-Z]*$/", $prorductCatagory)) {
-    header("Location: ./ProductName.php?=invalidproduct");
-    exit();
-} else if (!preg_match("/^[0-9]*$/", $productPrice)) {
-    header("Location: ./new_product?price=Invalidprice");
-    exit();
-  } else if (!preg_match("/^[0-9]*$/", $stock)) {
-      header("Location: ./new_product?price=Invalidprice");
-      exit();
-    // Får forbindels til databasen og får data med navne
-} else {
-    $sql = "SELECT * FROM webshop.product WHERE productName='$productName'";
-    $result = $conn->query($sql);
-    $resultCheck = mysqli_num_rows($result);
-
-    // Tjekker for om navnet er taget i databasen
-    if ($resultCheck > 0) {
-        header("location: ./new_product.php?product=exist");
+    //Tjekker for tomme felter
+    if (empty($name) || empty($category) || empty($price) || empty($stock)) {
+        header("location: ./new_product.php?=empty");
         exit();
+    } else if (!preg_match("/^[a-zA-Z]*$/", $name) || !preg_match("/^[a-zA-Z]*$/", $prorductCatagory)) {
+        header("Location: ./ProductName.php?=invalidproduct");
+        exit();
+    } else if (!preg_match("/^[0-9]*$/", $price)) {
+        header("Location: ./new_product?price=Invalidprice");
+        exit();
+    } else if (!preg_match("/^[0-9]*$/", $stock)) {
+        header("Location: ./new_product?price=Invalidprice");
+        exit();
+        // Får forbindels til databasen og får data med navne
     } else {
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        //Tilføj dataen i databasen
-        $sql = "INSERT INTO webshop.brugere (ProductName, ProrductCatagory, Productprice, stock)
-        VALUES ('$productName', '$ProrductCatagory', '$Productprice', '$Stock')";
-        // Efter oprettelsen af bruger logger man ind
-        if ($conn->query($sql) === false) {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-            exit();
+        $sql = "SELECT * FROM webshop.products WHERE name='$name'";
+        $result = $conn->query($sql);
+        $resultCheck = mysqli_num_rows($result);
 
-            } else {
+        if ($resultCheck > 0) {
+            header("location: ./new_product.php?product=exist");
+            exit();
+        } else {
+            $sql = "INSERT INTO webshop.products (name, category, price, stock)
+          VALUES ('$name', '$category', '$price', '$Stock')";
+
+            if (!connect()->query($sql)) {
                 echo "Error: " . $sql . "<br>" . $conn->error;
                 exit();
             }
         }
     }
-} else {
-  header("location: ./productname.php");
-  exit();
 }
+
+header("location: ./productname.php");
+exit();
--- src/new_user.php
+++ src/new_user.php
@@ -2,56 +2,56 @@
 include('database.php');
 
 if (isset($_POST['submit'])) {
-$firstname = mysqli_real_escape_string(connect(), $_POST['firstname']);
-$lastname = mysqli_real_escape_string(connect(), $_POST['lastname']);
-$email = mysqli_real_escape_string(connect(), $_POST['email']);
-$username = mysqli_real_escape_string(connect(), $_POST['username']);
-$password = mysqli_real_escape_string(connect(), $_POST['password']);
-$balance = mysqli_real_escape_string(connect(), $_POST['balance']);
+    $firstname = mysqli_real_escape_string(connect(), $_POST['firstname']);
+    $lastname = mysqli_real_escape_string(connect(), $_POST['lastname']);
+    $email = mysqli_real_escape_string(connect(), $_POST['email']);
+    $username = mysqli_real_escape_string(connect(), $_POST['username']);
+    $password = mysqli_real_escape_string(connect(), $_POST['password']);
+    $balance = mysqli_real_escape_string(connect(), $_POST['balance']);
 
-if (empty($firstname) || empty($lastname) || empty($email) || empty($username) || empty($password)) {
-    header("location: ./signup.php?=empty");
-    exit();
-} else if (!preg_match("/^[a-zA-Z]*$/", $firstname) || !preg_match("/^[a-zA-Z]*$/", $lastname)) {
-    header("Location: ./signup.php?=invalidname");
-    exit();
-} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    header("Location: ./signup.php?signup=invalidemail");
-    exit();
-} else if (preg_match("/(admin)/", $username)) {
-    header("Location: ./signup.php?signup=invalidusername");
-    exit();
-} else {
-    $sql = "SELECT * FROM webshop.users WHERE email='$email'";
-    $result = connect()->query($sql);
-
-    // Tjekker for om emailen er taget i databasen (om der er flere end 0)
-    if (mysqli_num_rows($result) > 0) {
-        header("location: ./signup.php?signup=usertaken");
+    if (empty($firstname) || empty($lastname) || empty($email) || empty($username) || empty($password)) {
+        header("location: ./signup.php?=empty");
+        exit();
+    } else if (!preg_match("/^[a-zA-Z]*$/", $firstname) || !preg_match("/^[a-zA-Z]*$/", $lastname)) {
+        header("Location: ./signup.php?=invalidname");
+        exit();
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: ./signup.php?signup=invalidemail");
+        exit();
+    } else if (preg_match("/(admin)/", $username)) {
+        header("Location: ./signup.php?signup=invalidusername");
         exit();
     } else {
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "SELECT * FROM webshop.users WHERE email='$email'";
+        $result = connect()->query($sql);
 
-        $sql = "INSERT INTO webshop.users (firstname, lastname, email, username, password, balance)
-        VALUES ('$firstname', '$lastname', '$email', '$username', '$hashed_password' , '$balance')";
-
-        if (!connect()->query($sql)) {
-            echo "Error: " . $sql . "<br>" . connect()->error;
+    // Tjekker for om emailen er taget i databasen (om der er flere end 0)
+        if (mysqli_num_rows($result) > 0) {
+            header("location: ./signup.php?signup=usertaken");
             exit();
         } else {
-            session_start();
-            $_SESSION["loggedin"] = true;
-            $_SESSION["firstname"] = $firstname;
-            $_SESSION["lastname"] = $lastname;
-            $_SESSION["username"] = $username;
-            $_SESSION["balance"] = $balance;
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-            header("location: home.php");
+            $sql = "INSERT INTO webshop.users (firstname, lastname, email, username, password, balance)
+          VALUES ('$firstname', '$lastname', '$email', '$username', '$hashed_password' , '$balance')";
+
+            if (!connect()->query($sql)) {
+                echo "Error: " . $sql . "<br>" . connect()->error;
+                exit();
+            } else {
+                session_start();
+                $_SESSION["loggedin"] = true;
+                $_SESSION["firstname"] = $firstname;
+                $_SESSION["lastname"] = $lastname;
+                $_SESSION["username"] = $username;
+                $_SESSION["balance"] = $balance;
+
+                header("location: home.php");
+            }
         }
     }
 }
-// Hvis man går ind på siden via URL kommer man tilbage til signup.php
-} else {
-  header("location: signup.php");
-  exit();
-}
+
+header("location: signup.php");
+exit();
+?>
