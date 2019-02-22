fd32b0ac7d6a24c5e5a8ae07381783d17eaa5e80
Zarkino <fhv2325@gmail.com>
2019-02-22 21:17:40 +0100

    Shopping cart now shows items in cart and updates the price.

--- src/cart.php
+++ src/cart.php
@@ -53,17 +53,37 @@ foreach($products as $productID) {
         </div>
 
         <div style="display:flex; justify-content:space-between; background-color:rgba(255, 255, 255, 0.7);">
-            <div style="width:30%;">
-                <?php product(1, "Name", "Description", "40"); ?>
-                <?php product(2, "Name", "Description", "80"); ?>
+            <div style="width:25%;">
+                <?php
+                $priceTotal = 0;
+                foreach($_SESSION['cart'] as $id) {
+                    $sql = "SELECT * FROM webshop.products WHERE productID =$id";
+
+                    $result = connect()->query($sql);
+
+                    while($row = $result->fetch_assoc()) {
+                        /*
+                        echo '<div class="container" style="width:100%; justify-content:space-between;" onclick="location.href=\'productpage.php?id='.$id.'\';">';
+                        echo '<img class="image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Antu_draw-cuboid.svg/500px-Antu_draw-cuboid.svg.png" width="15%">';
+                        echo '<h2>'.$row['name'].'</h2><a>'.$row['price'].' kr.</a>';
+                        echo '</div>';
+                        */
+                        product($row['productID'], $row['name'], $row['description'], $row['price']);
+                        $priceTotal += $row['price'];
+                    }
+                }
+                ?>
+
+                <!--<?php product(1, "Name", "Description", "40"); ?>-->
+                <!--<?php product(2, "Name", "Description", "80"); ?>-->
                 <!--Function to list all products in shopping cart-->
                 <!--Sum all the items in the list-->
             </div>
 
             <br>
 
-            <div style="display:flex; flex-direction:column; align-items:center; align-content:space-between; justify-content:flex-end; width:30%;">
-                <a id="nohover" style="color:black;">Total: 120 kr.</a>
+            <div style="display:flex; flex-direction:column; align-items:center; align-content:space-between; justify-content:flex-start; width:30%;">
+                <a id="nohover" style="color:black;"><?php echo $priceTotal; ?> kr.</a>
                 <button>Proceed to checkout</button>
             </div>
         </div>
