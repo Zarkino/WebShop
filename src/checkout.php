edc9d411109fab51b99698b818a8ace0c4cd4c52
Zarkino <fhv2325@gmail.com>
2019-04-04 17:38:35 +0200

    Some checkout changes

--- src/checkout.php
+++ src/checkout.php
@@ -4,44 +4,42 @@ include('database.php');
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <LINK rel="stylesheet" type="text/css" href="../Styles/Style.css">
-    <META charset="utf-8">
-    <TITLE>Webshop</TITLE>
-    <!--<LINK rel="icon" type="image/gif" href="../Icons/dollar.png"/>-->
+    <head>
+        <LINK rel="stylesheet" type="text/css" href="../Styles/Style.css">
+        <META charset="utf-8">
+        <TITLE>Webshop</TITLE>
+        <!--<LINK rel="icon" type="image/gif" href="../Icons/dollar.png"/>-->
 
-    <script>
-    </script>
-</head>
+        <script>
+        </script>
+    </head>
 
-<body>
-<?php banner(); ?>
+    <body>
+        <?php banner(); ?>
 
-<br>
+        <br>
 
-<div style="background-color:rgba(255, 255, 255, 0.7);">
-    <h2 style="color:black;">Checkout</h2>
+        <div style="background-color:rgba(255, 255, 255, 0.7);">
+            <h2 style="color:black;">Your Information</h2>
 
-    <form action="new_user.php" method="POST">
-        <a style="color:black">Contact Information</a><br>
-        <input type="text" name="firstname" placeholder="Firstname" required>
-        <input type="text" name="lastname" placeholder="Lastname" required><br>
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="tel" name="phone" placeholder="Phone"><br>
-        <input type="hidden" name="url" value="checkout.php">
-        <button type="submit" name="submit">Buy</button>
-    </form>
+            <a style="color:black">Account</a><br>
+            <form action="new_user.php" method="POST">
+                <input type="text" name="firstname" placeholder="Firstname" required>
+                <input type="text" name="lastname" placeholder="Lastname" required><br>
+                <input type="email" name="email" placeholder="Email" required>
+                <!--<input type="tel" name="phone" placeholder="Phone"> --><!-- Not used, but could be useful -->
+                <input type="hidden" name="url" value="checkout.php"><br>
+                <button type="submit" name="submit">Confirm and pay</button>
+            </form>
 
-    <br>
+            <?php
+            if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
+                buy($_SESSION['userID'], $_SESSION['cart']);
+                header('location: account.php');
+            }
+            ?>
+        </div>
 
-    <?php
-    if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === TRUE) {
-        buy($_SESSION['userID'], $_SESSION['cart']);
-        header('location: account.php');
-    }
-    ?>
-</div>
-
-<br>
-</body>
+        <br>
+    </body>
 </html>
