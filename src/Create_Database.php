f2aab1d36f0265e38d6538ae5f3651c6ae12da33
Zarkino <fhv2325@gmail.com>
2019-04-06 16:44:45 +0200

    Shortened Create Database code

--- src/Create_Database.php
+++ src/Create_Database.php
@@ -1,15 +1,9 @@
 <?php
 require_once('database.php');
 
-$sql = "CREATE DATABASE IF NOT EXISTS webshop";
+$sql = "CREATE DATABASE IF NOT EXISTS webshop COLLATE latin1_danish_ci";
 
 if (connect()->query($sql)) {
-    echo 'Error creating database: ' . connect()->error;
-}
-
-$sql = "ALTER DATABASE webshop COLLATE latin1_danish_ci";
-
-if (!connect()->query($sql)) {
     echo 'Error creating database: ' . connect()->error;
 }
 
