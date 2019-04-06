ecbf874fd20a5e5fc6c1e4eed612aa11e260d21a
Zarkino <fhv2325@gmail.com>
2019-04-06 16:34:59 +0200

    Some changes to new_product

--- src/new_product.php
+++ src/new_product.php
@@ -3,6 +3,7 @@ include('database.php');
 
 if (isset($_POST['submit'])) {
     $name = connect()->real_escape_string($_POST['name']);
+    $image = connect()->real_escape_string($_POST['image']);
     $category = connect()->real_escape_string($_POST['category']);
     $price = connect()->real_escape_string($_POST['price']);
     $stock = connect()->real_escape_string($_POST['stock']);
@@ -12,7 +13,7 @@ if (isset($_POST['submit'])) {
         header("location: ./new_product.php?=empty");
         exit();
     } else if (!preg_match("/^[a-zA-Z]*$/", $name) || !preg_match("/^[a-zA-Z]*$/", $category)) {
-        header("location: ./ProductName.php?=invalidproduct");
+        header("location: ./ProductName.php?=Invalidproduct");
         exit();
     } else if (!preg_match("/^[0-9]*$/", $price)) {
         header("location: ./new_product?price=Invalidprice");
@@ -20,26 +21,25 @@ if (isset($_POST['submit'])) {
     } else if (!preg_match("/^[0-9]*$/", $stock)) {
         header("location: ./new_product?price=Invalidprice");
         exit();
-        // Får forbindels til databasen og får data med navne
     } else {
-        $sql = "SELECT * FROM webshop.products WHERE name='$name'";
-        $result = $conn->query($sql);
+        $sql = "SELECT * FROM webshop.products WHERE name=".$name;
+        $result = connect()->query($sql);
         $resultCheck = mysqli_num_rows($result);
 
         if ($resultCheck > 0) {
             header("location: ./new_product.php?product=exist");
             exit();
         } else {
-            $sql = "INSERT INTO webshop.products (name, category, price, stock)
-          VALUES ('$name', '$category', '$price', '$stock')";
+            $sql = "INSERT INTO webshop.products (name, image, category, price, stock)
+          VALUES ('$name', null, '$category', '$price', '$stock')";
 
             if (!connect()->query($sql)) {
-                echo "Error: " . $sql . "<br>" . $conn->error;
+                echo 'Error: ' . connect()->error;
                 exit();
             }
         }
     }
 }
-
-header("location: ./productname.php");
+header("location: home.php");
 exit();
+?>
